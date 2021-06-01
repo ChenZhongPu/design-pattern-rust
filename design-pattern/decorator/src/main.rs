45855ff27cd2c4b71528ddede7bfa39befaf7484
Zhongpu Chen <chenloveit@gmail.com>
2021-06-01 12:48:41 +0800

    decorator design

---
+++ design-pattern/decorator/src/main.rs
@@ -0,0 +1,10 @@
+use decorator::*;
+
+fn main() {
+    let beverage = Espresso;
+    println!("Espresso: {}", beverage.cost());
+    let mut mocha = Mocha::new(Box::new(beverage));
+    mocha = Mocha::new(Box::new(mocha));
+    println!("Espresso with 2 mocha: {}", mocha.cost());
+
+}
