07173775551c4aab1cf38980d9c747b2ee14b337
chenzhongpu <chenloveit@gmail.com>
2021-06-06 18:17:59 +0800

    remove old code

--- design-pattern/observer/src/lib.rs
+++
@@ -1,9 +0,0 @@
-mod observer;
-mod subject;
-
-pub use observer::Observer;
-pub use observer::CurrentConditionDisplay;
-pub use observer::PredictDisplay;
-
-pub use subject::Subject;
-pub use subject::WeatherData;
--- design-pattern/strategy/README.md
+++
@@ -1,1 +0,0 @@
-> `The Strategy Pattern` defines a family of algorithms, encapsulates each one, and makes them interchangeable. Strategy lets the algorithm vary independently from clients that use it.
