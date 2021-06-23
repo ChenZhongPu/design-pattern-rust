22b66d6f6e9204947f1378d5a60b22a6f4906898
Zhongpu Chen <chenloveit@gmail.com>
2021-06-23 16:50:02 +0800

    update facade pattern

--- structural/facade/src/home_theater.rs
+++ structural/facade/src/home_theater.rs
@@ -3,28 +3,28 @@ use std::cell::RefCell;
 use std::rc::Rc;
 
 pub struct HomeTheaterFacade {
-    amp: RefCell<Amplifier>,
+    amp: Amplifier,
     tuner: Rc<RefCell<Tuner>>,
 }
 
 impl HomeTheaterFacade {
     pub fn new(amp: Amplifier, tuner: Tuner)-> Self {
-       HomeTheaterFacade { amp: RefCell::new(amp), tuner: Rc::new(RefCell::new(tuner)) }
+       HomeTheaterFacade { amp, tuner: Rc::new(RefCell::new(tuner)) }
     }
 
-    pub fn listen_radio(&self, frequency: f64) {
+    pub fn listen_radio(&mut self, frequency: f64) {
         println!("Tuning in the airwaves...");
         self.tuner.borrow().on();
         self.tuner.borrow_mut().set_frequency(frequency);
-        self.amp.borrow().on();
-        self.amp.borrow_mut().set_volume(5);
-        self.amp.borrow_mut().set_tuner(Rc::clone(&self.tuner));
+        self.amp.on();
+        self.amp.set_volume(5);
+        self.amp.set_tuner(Rc::clone(&self.tuner));
     }
 
     pub fn stop_radio(&self) {
         println!("Shutting down the radio...");
         self.tuner.borrow().off();
-        self.amp.borrow().off();
+        self.amp.off();
     }
 
 }
